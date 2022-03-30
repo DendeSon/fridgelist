75b73db06f8e7b533443ab3c4ce8109b84c601c0
DendeSon <1341511+DendeSon@users.noreply.github.com>
2022-03-30 00:28:01 -0500

    Update fridgeList.py
    - General cleanup
    - Added 'Exit' function
    - Added short forms of input to some prompts
    - Fixed a bug where program would not stop when user called 'exit'
    - Added 2 functions, scanRecipes and printRecipes, to return which 'recipes' could be used with existing 'ingredients'

--- fridgeList.py
+++ fridgeList.py
@@ -1,14 +1,17 @@
-""" 
-    Issues so far:
-        - 'exit' does not always stop the program
-        - add/removeIngredient needs an exception for 'exit' input(changing flag bool and loop)
 """
+
+Personal project to help with fridge/pantry stock, choosing recipes, keeping track of expiry dates, etc...
+
+"""
+
 
 sorry = "Sorry, that input is not recognized."
 bye = "Goodbye!"
 notYet = "This function is not implemented yet."
 
-currentIngredients = ["milk", "bread", "rice"]
+###Stock###
+
+currentIngredients = ["milk", "bread", "rice", "butter", "eggs"]
 snackList = []
 
 ###Recipes###
@@ -16,7 +19,8 @@ snackList = []
 breakfast = {
     "cerealBowl" : ["milk", "cereal"],
     "toast" : ["bread", "butter"],
-    "eggsBacon" : ["eggs", "bacon"]
+    "eggsBacon" : ["eggs", "bacon"],
+    "frenchToast" : ["bread", "eggs"]
 }
 
 lunch = {}
@@ -29,94 +33,113 @@ dinner = {
     "ramenNoodles" : "ramen"
 }
 
+###Start Function###
+
 def Objective():
     userInput = input("What would you like to do? ingredients/recipes/exit ").lower()
     if userInput == "exit":
-        print(bye)
-        global flag
-        flag = False
-    elif userInput == "ingredients":
+        Exit()
+        # print(bye)
+        # global flag
+        # flag = False
+    elif userInput == "ingredients" or userInput == "i":
         Ingredient()
-    elif userInput == "recipes":
+    elif userInput == "recipes" or userInput == "r":
         Recipes()
     else:
         print(sorry)
-        Objective()        
+        Objective()
+
+###Stop Function###
+
+def Exit():
+    print(bye)
+    global flag
+    flag = False
+    return flag
         
 ###Ingredients Section###
 
 def Ingredient():
-    userIngredientsInput = input("Would you like to view the 'list', 'add' or 'remove' ingredients? add/remove/list/back/exit ").lower()
+    userIngredientsInput = input("Would you like to 'view', 'add' or 'remove' ingredients? add/remove/list/back/exit ").lower()
     if userIngredientsInput == "add":
         addIngredient()
-        Ingredient()
     elif userIngredientsInput == "remove":
         removeIngredient()
-        Ingredient()
-    elif userIngredientsInput == "list":
-        print(currentIngredients)
+    elif userIngredientsInput == "view":
+        print(f"Current List of Ingredients:\n{currentIngredients}")
         Ingredient()
     elif userIngredientsInput == "back":
+        print("Okay,")
         Objective()
     elif userIngredientsInput == "exit":
-        print(bye)
-        global flag
-        flag = False
+        Exit()
     else:
         print(sorry)
         Ingredient()
 
 def removeIngredient():
-    removeInput = input("What ingredient would you like to remove? ").lower()
-    try:
-        currentIngredients.remove(removeInput)
-        print(f"Removed '{removeInput}'!")
-    except ValueError:
-        print(f"'{removeInput}' is not in the ingredients lists!")
-    finally:
+    removeInput = input("What ingredient would you like to remove? ").lower()       
+    if removeInput == "back":
+        print("Okay,")
         Ingredient()
+    elif removeInput == "exit":
+        Exit()
+    else:
+        try:
+            currentIngredients.remove(removeInput)
+            print(f"Removed '{removeInput}'!")
+        except ValueError:
+            print(f"'{removeInput}' is not in the ingredients lists!")
+        finally:
+            Ingredient()
         
 def addIngredient():
     addInput = input("What ingredient would you like to add? ").lower()
+    if addInput == "back":
+        print("Okay,")
+        Ingredient()
+    elif addInput == "exit":
+        Exit()
+    else:
+        currentIngredients.append(addInput)
+        print(f"Added '{addInput}'!")
+        Ingredient()
     # sellByInput = input("Does this ingredient have a 'Sell By' date? ").lower() #Specify format mm/dd/yy
     # snackInput = input(f"Is '{addInput}' a snack?").lower() 
-    currentIngredients.append(addInput)
-    print(f"Added '{addInput}'!")
-    Ingredient()
     
 ###Recipes Section###  
     
 def Recipes():
-    userRecipeInput = input("Which meal would you like to view? breakfast/lunch/dinner/snacks/back/exit ").lower()
-    if userRecipeInput == "breakfast":
+    userRecipeInput = input("Which meal would you like to access? breakfast/lunch/dinner/snacks/back/exit ").lower()
+    if userRecipeInput == "breakfast" or userRecipeInput == "b":
         breakfast_recipes()
-    elif userRecipeInput == "lunch":
+    elif userRecipeInput == "lunch" or userRecipeInput == "l":
         lunch_recipes()
-    elif userRecipeInput == "dinner":
+    elif userRecipeInput == "dinner" or userRecipeInput == "d":
         dinner_recipes()
-    elif userRecipeInput == "snacks":
+    elif userRecipeInput == "snacks" or userRecipeInput == "s":
         snacks()
     elif userRecipeInput == "back":
+        print("Okay,")
         Objective()
     elif userRecipeInput == "exit":
-        print(bye)
-        global flag
-        flag = False
+        Exit()
     else:
         print(sorry)
         Recipes()
     
 
 def breakfast_recipes():
-    print(notYet)
+    printRecipes(breakfast)
     Recipes()
     
 def lunch_recipes():
-    print(notYet)
+    printRecipes(lunch)
     Recipes()
     
 def dinner_recipes():
-    print(notYet)
+    printRecipes(dinner)
     Recipes()
 
 def snacks():
@@ -130,22 +153,23 @@ def addRecipe():
 def removeRecipe():
     print(notYet)
     Recipes()
-    
-"""
-    In progress, function to check if a recipe has all required ingredients
-"""
 
-def scanRecipes(): #meal = breakfast/lunch/dinner
-        print(notYet)
-        Recipes()
-#     for item in currentIngredients:
-#         print(item)
-#         if item in breakfast.values():
+def scanRecipes(meal): #meal = breakfast/lunch/dinner
+    return {
+        r: i for r, i in meal.items()
+        if set(currentIngredients).issuperset(set(i))
+        }
+    
+def printRecipes(meal):
+    print("Recipes with in stock ingredients:")
+    for r, i in scanRecipes(meal).items():
+        print(f"{r} with {i}")
+    
             
     
-###General Loops###
+###Program Loop###
 
-flag = True   
- 
+flag = True
+
 while flag:
     Objective()
